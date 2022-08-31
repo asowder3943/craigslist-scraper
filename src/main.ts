ff861b2730d53e1481482394131f6e9ae561465b
= <asowder3943@gmail.com>
2022-08-31 04:56:44 -0400

    🚸 Updated input schema

--- src/main.ts
+++ src/main.ts
@@ -3,66 +3,23 @@
 
 // For more information, see https://sdk.apify.com/
 import { Actor } from 'apify';
-import { CraigslistSearchInput, buildRequestUrls, CraigslistPost } from "./craigslist.js";
-import { PlaywrightCrawler } from 'crawlee'
-import { strict as assert } from "assert";
 
-interface InputSchema {
-    location: string[] | undefined
+interface LocationInputSchema {
+    site: string[] | undefined
+    geoLocation: string[] | undefined
+    zip: string[] | undefined
+}
+
+interface SearchInputSchema {
     category: string[] | undefined
     query: string[] | undefined
+    titleOnly: Boolean
 }
 
 await Actor.init()
-
-// const proxyConfiguration = await  Actor.createProxyConfiguration({
-//     countryCode: 'US'
-// });
-
 console.log('Loading input');
-// Structure of input is defined in INPUT_SCHEMA.json.
-const input = await Actor.getInput<InputSchema>() as CraigslistSearchInput;
-
-
-const crawler = new PlaywrightCrawler({
-    maxRequestsPerCrawl: 10,
-    headless: false,
-    requestHandler: async ({ page, request }) => {
-        console.log(`Scraping ${await page.title()} | ${request.url}`);
-
-    const _titles = await page.$$eval(".result-title", (els: any[]) => {
-      return els.map((el) => el.textContent);
-    });
-
-    const _urls = await page.$$eval(".result-title", (els: any[]) => {
-      return els.map((el) => el.getAttribute("href"));
-    });
-
-    const _dates = await page.$$eval(".result-date", (els: any[]) => {
-      return els.map((el) => el.getAttribute("datetime"));
-    });
-
-    try{
-        assert.equal(_titles.length, _urls.length, `The number of titles found does not match the number of urls found`)
-        assert.equal(_urls.length, _dates.length, `The number of titles found does not match the number of urls found`)
-    }
-    catch(AssertionError){
-        console.warn(`${AssertionError}`)
-    }
-
-    var _posts: CraigslistPost[] = []
-    
-    for(var i = 0; i < _titles.length; i++){
-        _posts.push({
-            url: _urls[i]!,
-            title: _titles[i]!,
-            date: _dates[i]!
-        })
-    }
-
-    await Actor.pushData(_posts)
-}})
-
-await crawler.run(buildRequestUrls(input));
-
+const locations = await Actor.getInput<LocationInputSchema>();
+const searches = await Actor.getInput<SearchInputSchema>();
+console.log(locations)
+console.log(searches)
 await Actor.exit();
