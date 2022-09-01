fa41e2a46556d2e71f9ad723a8e1958a6c3acb2e
= <asowder3943@gmail.com>
2022-09-01 09:10:09 -0400

    ♻️ Created Dev Scraper and broke Tests to new file

---
+++ src/scraper-dev.ts
@@ -0,0 +1,51 @@
+import { PlaywrightCrawler, Dataset } from "crawlee";
+import { strict as assert } from "assert";
+import { CraigslistPost } from "./types.js";
+
+export const crawler = new PlaywrightCrawler({
+  maxRequestsPerCrawl: 10,
+    headless: false,
+    requestHandler: async ({ page, request }) => {
+      console.log(`Scraping ${await page.title()} | ${request.url}`);
+      const dataset = await Dataset.open();
+
+      const _titles = await page.$$eval(".result-title", (els: any[]) => {
+        return els.map((el) => el.textContent);
+      });
+
+      const _urls = await page.$$eval(".result-title", (els: any[]) => {
+        return els.map((el) => el.getAttribute("href"));
+      });
+
+      const _dates = await page.$$eval(".result-date", (els: any[]) => {
+        return els.map((el) => el.getAttribute("datetime"));
+      });
+
+      try {
+        assert.equal(
+          _titles.length,
+          _urls.length,
+          `The number of titles found does not match the number of urls found`
+        );
+        assert.equal(
+          _urls.length,
+          _dates.length,
+          `The number of titles found does not match the number of urls found`
+        );
+      } catch (AssertionError) {
+        console.warn(`${AssertionError}`);
+      }
+
+      var _posts: CraigslistPost[] = [];
+
+      for (var i = 0; i < _titles.length; i++) {
+        _posts.push({
+          url: _urls[i]!,
+          title: _titles[i]!,
+          date: _dates[i]!,
+        });
+      }
+
+      await dataset.pushData(_posts);
+    },
+  });
---
+++ src/tests.ts
@@ -0,0 +1,136 @@
+import { InputSchema } from "./types.js";
+import { ApifyInputError, getRequestUrlsFromInput } from "./validation.js";
+
+function runTestCases(cases: InputSchema[]) {
+  var _i = 1;
+  for (var _case in cases) {
+    console.info(`Running Test Case ${_i}`);
+    try {
+      var _testResult = getRequestUrlsFromInput(cases[_case]);
+      console.log(_testResult);
+    } catch (e) {
+      if (e instanceof ApifyInputError) {
+        console.warn(`Test Case Failure: \n${e}`);
+      } else {
+        console.warn(`Unexpected Error: \n${e}`);
+      }
+    }
+    console.info(`\n\n\n`);
+    _i++;
+  }
+}
+
+var siteValidationTestCases: InputSchema[] = [
+  {
+    site: ["AUBURN", "roanoke", "charlottesville"],
+  },
+  {
+    site: ["invalid_name"],
+  }
+];
+
+var geoLocationValidationTestCases: InputSchema[] = [
+  {
+    geoLocation: ["32.609856, -85.480782", "32.609856, -85.480782, 12"],
+  },
+  {
+    geoLocation: ["32.1"],
+  },
+  {
+    geoLocation: ["21, -60, 12, 43"],
+  },
+  {
+    geoLocation: ["-100, -80"],
+  },
+  {
+    geoLocation: ["100, -80"],
+  },
+  {
+    geoLocation: ["32, -200"],
+  },
+  {
+    geoLocation: ["32, 200"],
+  },
+];
+
+var zipCodeValidationTestCases: InputSchema[] = [
+  {
+    zipCode: ["22902", "24018, 12"],
+  },
+  {
+    zipCode: ["22.02"],
+  },
+  {
+    zipCode: ["22903, 12, 34"],
+  },
+  {
+    zipCode: ["12"],
+  },
+  {
+    zipCode: ["1290321"],
+  },
+  {
+    zipCode: ["derfr"],
+  },
+];
+
+
+var categoryValidationTestCases: InputSchema[] = [
+  {
+    category: ['RRR', 'ggg']
+  },
+  {
+    category: ['invalid category']
+  }
+]
+
+var queryValidationTestCases: InputSchema[] = [
+  {
+    query: ['programmer', 'software & developer', 'software & engineer']
+  },
+  {
+    query: []
+  },
+  {
+    query: ['']
+  },
+  {
+    query: [' ', '   ']
+  },
+  {
+    query: ['"SpecficText"']
+  },
+  {
+    query: ['WildC*rd']
+  },
+  {
+    query: ['bike -kids']
+  },
+  {
+    query: ['programmer', 'software & developer', 'software & engineer', 'coding', 'java', 'python', 'excel', 'vba'],
+  }
+]
+
+var comboValidationTestCases: InputSchema[] = [
+  {
+    site: ["AUBURN", "roanoke", "charlottesville"],
+    geoLocation: ["32.609856, -85.480782", "32.609856, -85.480782, 12"],
+    zipCode: ["22902", "24018, 12"],
+    category: ['RRR', 'ggg'],
+    query: ['programmer', 'software & developer', 'software & engineer'],
+    urls: ['https://charlottesville.craigslist.org/search/msa?query=guitar&min_price=&max_price=100']
+  }
+]
+
+var realWorld: InputSchema[] = [
+  {
+    category: ['ggg'],
+  }
+]
+
+runTestCases(siteValidationTestCases);
+runTestCases(geoLocationValidationTestCases);
+runTestCases(zipCodeValidationTestCases);
+runTestCases(categoryValidationTestCases);
+runTestCases(queryValidationTestCases);
+runTestCases(comboValidationTestCases);
