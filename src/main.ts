44286399e658bfd7acd29a40d5ad51907181c0df
= <asowder3943@gmail.com>
2022-10-04 15:25:54 -0400

    testing deployment

--- src/main.ts
+++ src/main.ts
@@ -10,4 +10,5 @@ const crawlerSetup = new CrawlerSetup(input!)
 const crawler = await crawlerSetup.getCrawler()
 console.info(`Crawler Initialization Complete: ${crawlerSetup.startUrls.length} urls have been added to the queue`)
 await crawler.run(crawlerSetup.startUrls)
+console.info('testing push')
 await Actor.exit();
