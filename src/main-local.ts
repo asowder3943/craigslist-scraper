bd3045dc974c93220569582618cad8c06a5e6ccb
asowder3943 <asowder3943@gmail.com>
2022-12-10 05:38:41 -0500

    updated npm config

--- src/main-local.ts
+++ src/main-local.ts
@@ -1,12 +1,20 @@
 import { Actor } from "apify";
 import { CrawlerSetup } from "./scraper.js";
 import { InputSchema } from "./types.js";
+import { Axios } from 'axios'
+const __workingInputCase: InputSchema = {
+  category: ["ggg"],
+  maxPagesPerCrawl: 100,
+  proxyConfiguration: {
+      useApifyProxy: false
+  },
+};
 
 await Actor.init();
 console.info("Loading input");
-const input = await Actor.getInput<InputSchema>();
+console.info(__workingInputCase);
 console.info('Initializing Craigslist Crawler')
-const crawlerSetup = new CrawlerSetup(input!)
+const crawlerSetup = new CrawlerSetup(__workingInputCase!)
 const crawler = await crawlerSetup.getCrawler()
 console.info(`Crawler Initialization Complete: ${crawlerSetup.startUrls.length} urls have been added to the queue`)
 await crawler.run(crawlerSetup.startUrls)
